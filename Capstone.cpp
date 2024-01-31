09e1397c194813de3af568e86182f5a655054042
Boonpin97 <pohboonpin123@hotmail.com>
2024-01-31 23:41:23 +0800

    Shorten loop duration in Tray::move for FSM

--- Capstone.cpp
+++ Capstone.cpp
@@ -1,10 +1,12 @@
 #include "Capstone.h"
 
 Tray::Tray(int stepPin, int dirPin, int backlimit, int frontlimit)
-  : stepPin(stepPin), dirPin(dirPin), backlimit(backlimit), frontlimit(frontlimit) {
+    : stepPin(stepPin), dirPin(dirPin), backlimit(backlimit), frontlimit(frontlimit)
+{
 }
 
-void Tray::begin() {
+void Tray::begin()
+{
   Wire.begin(SDA_PIN, SCL_PIN);
 
   pinMode(stepPin, OUTPUT);
@@ -12,63 +14,81 @@ void Tray::begin() {
   pinMode(backlimit, INPUT);
   pinMode(frontlimit, INPUT);
 
-  for (int i = 0; i < NUM_LIGHT_SENSOR; i++) {
+  for (int i = 0; i < NUM_LIGHT_SENSOR; i++)
+  {
     tcaselect(i);
-    if (!light_sensor[i].begin()) {
+    if (!light_sensor[i].begin())
+    {
       Serial.print("Light sensor ");
       Serial.print(i);
       Serial.println(" not found");
       Serial.println("WARNING: CODE MIGHT CRASH IF THE ERROR ABOVE IS IGNORED");
-      delay(2000);
+      // delay(2000);
     }
-    Serial.print("Light sensor ");
-    Serial.print(i);
-    Serial.println(" found!");
-    light_sensor[i].setLowThreshold(10000);
-    light_sensor[i].setHighThreshold(20000);
-    light_sensor[i].interruptEnable(true);
+    else
+    {
+      Serial.print("Light sensor ");
+      Serial.print(i);
+      Serial.println(" found!");
+      light_sensor[i].setLowThreshold(10000);
+      light_sensor[i].setHighThreshold(20000);
+      light_sensor[i].interruptEnable(true);
+    }
   }
 }
 
-int Tray::move(int target_pos, int spd) {
-  int diff_pos = (target_pos - current_pos) * STEPS_PER_REV / (3.1415 * GEAR_DIAMETER);
+int Tray::move(int target_pos, int spd)
+{
+  float K = STEPS_PER_REV / (3.1415 * GEAR_DIAMETER);
   bool dir;
-
-  if (diff_pos != 0) {
-    if (diff_pos > 0) {
+  if (target_pos - current_pos != 0)
+  {
+    if (target_pos - current_pos > 0)
+    {
       dir = true;
-      digitalWrite(dirPin, HIGH);
-    } else {
-      dir = false;
       digitalWrite(dirPin, LOW);
     }
-
-    current_pos = target_pos;
-
-    for (int i = 0; i < abs(diff_pos); i++) {
-      if ((digitalRead(frontlimit) || !dir) && (digitalRead(backlimit) || dir)) {
-        if (digitalRead(frontlimit)) {
+    else
+    {
+      dir = false;
+      digitalWrite(dirPin, HIGH);
+    }
+    for (int i = 0; i < K; i++)
+    {
+      bool front_pressed = digitalRead(frontlimit);
+      bool back_pressed = digitalRead(backlimit);
+      if ((front_pressed || !dir) && (back_pressed || dir))
+      {
+        if (front_pressed)
+        {
           current_pos = STROKE_LENGTH;
-        } else if (digitalRead(backlimit)) {
+        }
+        else if (back_pressed)
+        {
           current_pos = 0;
         }
         break;
-      } else {
+      }
+      else
+      {
         digitalWrite(stepPin, HIGH);
         delayMicroseconds(spd);
         digitalWrite(stepPin, LOW);
         delayMicroseconds(spd);
       }
     }
-
-    return current_pos;
+    if (!(digitalRead(frontlimit) || digitalRead(backlimit)))
+      current_pos++;
   }
+  return current_pos;
 }
 
-int Tray::resetFront(int spd) {
-  digitalWrite(dirPin, HIGH);
+int Tray::resetFront(int spd)
+{
+  digitalWrite(dirPin, LOW);
 
-  while (!digitalRead(frontlimit)) {
+  while (!digitalRead(frontlimit))
+  {
     digitalWrite(stepPin, HIGH);
     delayMicroseconds(spd);
     digitalWrite(stepPin, LOW);
@@ -79,10 +99,12 @@ int Tray::resetFront(int spd) {
   return current_pos;
 }
 
-int Tray::resetBack(int spd) {
-  digitalWrite(dirPin, LOW);
+int Tray::resetBack(int spd)
+{
+  digitalWrite(dirPin, HIGH);
 
-  while (!digitalRead(backlimit)) {
+  while (!digitalRead(backlimit))
+  {
     digitalWrite(stepPin, HIGH);
     delayMicroseconds(spd);
     digitalWrite(stepPin, LOW);
@@ -93,17 +115,21 @@ int Tray::resetBack(int spd) {
   return current_pos;
 }
 
-float* Tray::readLuxs() {
+float *Tray::readLuxs()
+{
   static float light_array[NUM_LIGHT_SENSOR];
-  for (int i = 0; i < NUM_LIGHT_SENSOR; i++) {
+  for (int i = 0; i < NUM_LIGHT_SENSOR; i++)
+  {
     tcaselect(i);
     light_array[i] = light_sensor[i].readLux();
   }
   return light_array;
-} 
+}
 
-void Tray::tcaselect(uint8_t i) {
-  if (i <= 7) {
+void Tray::tcaselect(uint8_t i)
+{
+  if (i <= 7)
+  {
     Wire.beginTransmission(0x71);
     Wire.write(0);
     Wire.endTransmission();
@@ -111,7 +137,8 @@ void Tray::tcaselect(uint8_t i) {
     Wire.write(1 << i);
     Wire.endTransmission();
   }
-  else if (i == 8) {
+  else if (i == 8)
+  {
     Wire.beginTransmission(0x70);
     Wire.write(0);
     Wire.endTransmission();
@@ -120,5 +147,4 @@ void Tray::tcaselect(uint8_t i) {
     Wire.endTransmission();
   }
   return;
-} 
-
+}
--- Capstone.h
+++ Capstone.h
@@ -4,30 +4,36 @@
 #include "Arduino.h"
 #include "Adafruit_VEML7700.h"
 
-#define DEFAULT_TRAY_SPEED 500
 #define STEP_PIN 33
 #define DIR_PIN 32
+#define ENABLE_PIN 25
 #define BACK_LIMIT_PIN 36
 #define FRONT_LIMIT_PIN 39
 #define SDA_PIN 21
 #define SCL_PIN 22
+#define GROWLIGHT_PIN 13
+#define WIFILIGHT_PIN 19
+#define RESET_PIN 0
 
-class Tray {
+class Tray
+{
 public:
     Tray(int stepPin = STEP_PIN, int dirPin = DIR_PIN, int backlimit = BACK_LIMIT_PIN, int frontlimit = FRONT_LIMIT_PIN);
     void begin();
     int move(int target_pos, int spd = DEFAULT_TRAY_SPEED);
     int resetFront(int spd = DEFAULT_TRAY_SPEED);
     int resetBack(int spd = DEFAULT_TRAY_SPEED);
-    float* readLuxs();
+    float *readLuxs();
 
 private:
-
     // Parameters
-    static constexpr float GEAR_DIAMETER = 19.15;  
-    static const int STEPS_PER_REV = 1600; 
-    static const int STROKE_LENGTH = 342;
-    static const int NUM_LIGHT_SENSOR = 9;
+    static const int DEFAULT_TRAY_SPEED = 500;
+    static constexpr float GEAR_DIAMETER = 19.15;
+    static const int STEPS_PER_REV = 1600;
+    static const int STROKE_LENGTH = 334;
+    static const int MIN_DELAY = 200;
+    static const int MAX_DELAY = 2200;
+    static const int NUM_LIGHT_SENSOR = 6;
     static const int LIGHT_LOW_THRESHOLD = 10000;
     static const int LIGHT_HIGH_THRESHOLD = 20000;
     static const bool LIGHT_INTERRUPT_ENABLE = true;
