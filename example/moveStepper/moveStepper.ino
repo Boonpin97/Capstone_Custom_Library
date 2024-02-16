a0b0b266257b155d554e1277eeeb1106adf6d02e
Boonpin97 <pohboonpin123@hotmail.com>
2024-02-16 13:40:37 +0800

    Added comments

--- example/moveStepper/moveStepper.ino
+++ example/moveStepper/moveStepper.ino
@@ -20,19 +20,19 @@ void setup() {
 void loop() {
   Serial.print("State: ");
   Serial.println(state);
-  if (state == 0) {
+  if (state == 0) { // State 0: Reset the tray to the 0 position
     if (myTray.resetBack() == true) {
       state++;
     }
   }
-  else if (state == 1) {
+  else if (state == 1) {  // State 1: Check for Serial Monitor input
     if (Serial.available() > 0) {
       target_pos = Serial.parseInt();                                 // Read integer input from serial monitor
       Serial.print("Target position: "); Serial.println(target_pos); // Print out the target position of the tray
       state++;
     }
   }
-  else if (state == 2) {
+  else if (state == 2) {  // State 2: Move the tray to the target position
     if (myTray.move(target_pos, 50) == true) {                                             // Move the tray to the target position
       state = 1;
     }
